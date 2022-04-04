4f2b4464a386284e3b9386a2e0907147f4542643
mald7690 <mald7690@mylaurier.ca>
2022-04-04 14:36:09 -0400

    Basic UI Updated
    Added basic UI functions for RQ, RL, Status, and Run.

--- Question1.c
+++ Question1.c
@@ -16,38 +16,75 @@ Programming Banker's Algorithm
 static int *available;
 static int maximum[5][4] = {{6,4,7,3},{4,2,3,2},{2,5,3,3},{6,3,3,2},{5,5,7,5}};
 static int *allocated;
-static int *need;
+static int need[5][4] = {{6,4,7,3},{4,2,3,2},{2,5,3,3},{6,3,3,2},{5,5,7,5}};
+static char terminate[5]={'e','x','i','t'};
+static char request[3]={'r','q'};
+static char release[3]={'r','l'};
+static char status[7]={'s','t','a','t','u','s'};
+static char run[4]={'s','u','n'};
+static int exit_cond = 1;
+static int holder[5];
+
+
+
 
 
 int main(int argc, char *argv[]){
 	//allocate memory to global arrays
 	available = malloc(sizeof(int)*4);
 	allocated = malloc(sizeof(int)*4);
-	need = malloc(sizeof(int)*4);
 
 	//populate available array
 	for(int i=1; i < argc; i++){
 		available[i] = atoi(argv[i]);
 	}
-	//Setup for exit conditions
-	char command[12]={0};
-	char terminate[5]={'e','x','i','t'};
-	int exit_cond = 1;
+		//Setup for exit conditions
+		char command[12]={0};
+		//main while loop
+		int j = 0;
+		while(exit_cond != 0){
+			printf("Enter a command:");
+			scanf(" %[^\n]s",command);
+			char* comm;
+			for(int i = 0; terminate[i];i++){
+				command[i]=tolower(command[i]);
+			}
+			//direct to one of if statements depending on command
 
-	//main while loop
-	while(exit_cond != 0){
-		printf("Enter a command:");
-		scanf("%s",command);
-		for(int i = 0; terminate[i];i++){
-			command[i]=tolower(command[i]);
+			char *comp;
+			int i = 0;
+			char* ret;
+
+			comp = strtok(command," ");
+			if(strcmp(terminate,comp)==0){
+				exit_cond=0;
+			}
+			else if(strcmp(comp,request)==0){
+				while(comp != NULL)
+				{
+					holder[i]=atoi(comp);
+					comp = strtok(NULL," ");
+					i++;
+				}
+			}
+			else if(strcmp(comp,release)==0){
+				while(comp != NULL)
+				{
+					holder[i]=atoi(comp);
+					comp = strtok(NULL," ");
+					i++;
+				}
+			}
+			else if(strcmp(comp,status)==0){
+				printf("Status");
+			}
+			else if(strcmp(comp,run)==0){
+				printf("Run");
+			}
+			else{
+				printf("Invalid input, use one of RQ, RL, Status, Run, Exit\n");
+			}
 		}
-		printf("%d",strcmp(terminate,command));
-		if(strcmp(terminate,command)==0){
-
-			exit_cond=0;
-		}
-
-	}
 
 }
 
